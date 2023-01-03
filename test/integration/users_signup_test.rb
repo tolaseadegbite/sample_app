3155b7796d5f134964b23adf42a0f4b12797e14f
tolaseadegbite <tolasekelvin@gmail.com>
2023-01-03 17:24:18 +0100

    Finish sign-up

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,31 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  
+  test "invalid user information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: {  name: "",
+                                          email: "foo@invalid",
+                                          password: "foo",
+                                          password_confirmation: "bar"}}
+    end
+    assert_response :unprocessable_entity
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert.alert-danger'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: {  name: "Foobar",
+                                          email: "foobar@example.com",
+                                          password: "foobar",
+                                          password_confirmation: "foobar"}}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select 'div.alert.alert-success'
+  end
+end
