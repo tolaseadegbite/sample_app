a0ae54c94a68b1be1d43fa112f652d1c7e85252a
tolaseadegbite <tolasekelvin@gmail.com>
2023-01-08 20:15:37 +0100

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,24 +1,52 @@
 module SessionsHelper
-    # Logs in the given user
+
+    # Logs in the given user.
     def log_in(user)
-        session[:user_id] = user.id
+      session[:user_id] = user.id
+      # Guard against session replay attacks.
+      # See https://bit.ly/33UvK0w for more.
+      session[:session_token] = user.session_token
     end
-
-    # Returns the current logged-in user (if any)
+  
+    # Remembers a user in a persistent session.
+    def remember(user)
+        user.remember
+        cookies.permanent.encrypted[:user_id] = user.id
+        cookies.permanent[:remember_token] = user.remember_token
+    end
+  
+    # Returns the user corresponding to the remember token cookie.
     def current_user
-        if session[:user_id]
-            @current_user ||= User.find_by(id: session[:user_id])
+      if (user_id = session[:user_id])
+        user = User.find_by(id: user_id)
+        if user && session[:session_token] == user.session_token
+          @current_user = user
         end
+      elsif (user_id = cookies.encrypted[:user_id])
+        user = User.find_by(id: user_id)
+        if user && user.authenticated?(cookies[:remember_token])
+          log_in user
+          @current_user = user
+        end
+      end
     end
-
-    # Returns true if the user is logged in, false otherwise
+  
+    # Returns true if the user is logged in, false otherwise.
     def logged_in?
-        !current_user.nil?
+      !current_user.nil?
     end
-
-    # Logs out the current user
+    
+    # Forget a persistent user
+    def forget(user)
+        user.forget
+        cookies.delete(:user_id)
+        cookies.delete(:remember_token)
+    end
+  
+    # Logs out the current user.
     def log_out
-        reset_session
-        @current_user = nil
+        forget(current_user)
+      reset_session
+      @current_user = nil
     end
-end
+  end
