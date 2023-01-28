7cae24b3203429bd999866843d8e2def3ae2041f
tolaseadegbite <tolasekelvin@gmail.com>
2023-01-28 21:35:29 +0100

    Add account activation without production

---
+++ test/integration/user_show_test.rb
@@ -0,0 +1,15 @@
+require "test_helper"
+
+class UserShowTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @inactive_user = users(:inactive)
+    @activated_user = users(:archer)
+  end
+
+  test "should redirect when user not activated" do
+    get user_path(@inactive_user)
+    # assert_response :unprocessable_entity
+    assert_redirected_to root_url
+  end
+end
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -12,7 +12,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
-    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users = User.where(activated: true).paginate(page: 1)
     first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
@@ -26,10 +26,23 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     end
   end
 
+  test "should display only activated users" do
+    # Deactivate the first user on the page.
+    # Making an inactive fixture user isn't sufficient because Rails can't
+    # guarantee it would appear on the first page.
+    log_in_as(@admin)
+    User.paginate(page: 1).first.toggle!(:activated)
+    # Re-get /users to confirm the deactivated user doesn't appear.
+    get users_path
+    # Ensure that all the displayed users are activated.
+    assigns(:users).each do |user|
+      assert user.activated?
+    end
+  end
+
   test "index as non-admin" do
     log_in_as(@non_admin)
     get users_path
     assert_select 'a', text: 'delete', count: 0
   end
-
 end
